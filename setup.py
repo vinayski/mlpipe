1736ee5375746347bf7ab2d0da36efa72d40dfff
SKi <vinayski@gmail.com>
2018-05-07 11:23:38 -0700

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mlpipe',
+	version='0.1',
+	description='mlpipe',
+	url='http://github.com/vinayski/mlpipe',
+	author='vinayski',
+	author_email='vinayski',
+	license='None',
+	packages=['mlpipe'],
+	zip_safe=False)
